212e747b9e96aacc3f9cad4ff0998520e02fc6c6
Victor J. Pudelski <vjpudelski@gmail.com>
2015-12-30 17:36:19 -0500

    Added Unity to the projects

---
+++ DependencyInjection/DIUnity/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DISamurai.Models;
+using Microsoft.Practices.Unity;
+
+namespace DIUnity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // basic constructor injection
+            var container = new UnityContainer();
+            container.RegisterType<IWeapon, Sword>();
+            var samurai = container.Resolve<Samurai>();
+
+            samurai.Attack("the evildoers");
+
+            var container2 = new UnityContainer();
+            container2.RegisterType<IWeapon, Shuriken>();
+            InjectionProperty injectionProperty = new InjectionProperty("SecondaryWeapon", new Sword());
+            container2.RegisterType<Samurai>(injectionProperty);
+            // overriding the parameter of the constructor.
+            var samurai2 = container2.Resolve<Samurai>(new ParameterOverride("weapon", new Sword()));
+
+            Console.ReadLine();
+        }
+    }
+}
